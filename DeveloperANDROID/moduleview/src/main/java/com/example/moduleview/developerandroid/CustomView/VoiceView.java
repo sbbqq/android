baffa8378e838e5636f155f5f8c6c394c408cabb
wqq <286402940@qq.com>
2018-03-13 14:06:56 +0800

    finish viewvoice.view

--- DeveloperANDROID/moduleview/src/main/java/com/example/moduleview/developerandroid/CustomView/VoiceView.java
+++ DeveloperANDROID/moduleview/src/main/java/com/example/moduleview/developerandroid/CustomView/VoiceView.java
@@ -9,6 +9,7 @@ import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.moduleview.R;
@@ -23,8 +24,12 @@ public class VoiceView  extends View {
     private int progress=30;//default show 30 percent
     private int swipe_unit;
     private float paintWidth=10;
+    private float paintwidthpro=10;
     Paint paintbg;
     Paint paintshow;
+    Paint paintpro;
+    RectF rectF;
+    private float startX,startY;
     public VoiceView(Context context) {
         super(context);
     }
@@ -41,6 +46,14 @@ public class VoiceView  extends View {
         paintshow.setStyle(Paint.Style.FILL_AND_STROKE);
         paintshow.setColor(Color.BLACK);
         paintshow.setStrokeWidth(paintWidth);
+        rectF=new RectF();
+        paintpro=new Paint();
+        paintpro.setTextSize(14);
+        paintpro.setStrokeCap(Paint.Cap.ROUND);
+        paintpro.setStyle(Paint.Style.FILL);
+        paintpro.setColor(Color.YELLOW);
+
+
         TypedArray typedArray=context.getTheme().obtainStyledAttributes(attrs, R.styleable.VoiceView,0,0);
         int attrindex;
         for(int i=0;i<typedArray.length();i++){
@@ -77,14 +90,22 @@ public class VoiceView  extends View {
         super(context, attrs, defStyleAttr);
         paintbg=new Paint();
         paintshow=new Paint();
+        rectF=new RectF();
+        paintbg.setAntiAlias(true);
         paintshow.setStrokeCap(Paint.Cap.ROUND);
         paintbg.setStrokeCap(Paint.Cap.ROUND);
         paintbg.setColor(Color.RED);
         paintbg.setStrokeWidth(paintWidth);
         paintbg.setStyle(Paint.Style.FILL_AND_STROKE);
-        paintshow.setStyle(Paint.Style.FILL_AND_STROKE);
+        paintshow.setStyle(Paint.Style.FILL);
         paintshow.setColor(Color.BLACK);
         paintshow.setStrokeWidth(paintWidth);
+        paintpro=new Paint();
+        paintpro.setStrokeCap(Paint.Cap.ROUND);
+
+        paintpro.setStyle(Paint.Style.FILL_AND_STROKE);
+        paintpro.setTextSize(14);
+        paintpro.setColor(Color.YELLOW);
         TypedArray typedArray=context.getTheme().obtainStyledAttributes(attrs, R.styleable.VoiceView,defStyleAttr,0);
         int attrindex;
         for(int i=0;i<typedArray.length();i++){
@@ -125,6 +146,7 @@ public class VoiceView  extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
     }
     public void drawAc(Canvas canvas){
         Log.e("drawAc","******************");
@@ -135,15 +157,62 @@ public class VoiceView  extends View {
 
 
         radus=Math.min(getMeasuredHeight()/2,getMeasuredWidth()/2);
+        radus=radus-paintWidth;
         Log.e("xy","x:"+x+"y:"+y+"radus:"+radus+"swipe_unit:"+swipe_unit+"count:"+countdop);
-        RectF rectF=new RectF(x-radus,y-radus,x+radus,y+radus);
+       rectF=new RectF(x-radus,y-radus,x+radus,y+radus);
 
         for(int i=0;i<countdop;i++){
             canvas.drawArc(rectF,i*(360/(float)countdop),swipe_unit,false,paintbg);
         }
         int showcount=countdop*progress/100;
-        for(int i=0;i<=showcount;i++){
+        for(int i=0;i<showcount;i++){
             canvas.drawArc(rectF,i*(360/(float)countdop),swipe_unit,false,paintshow);
         }
+        paintpro.setTextSize(50);
+        paintpro.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(""+progress,rectF.centerX(),rectF.centerY(),paintpro);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                if(rectF.contains(event.getX(),event.getY())) {
+                    startX = event.getX();
+
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+                if(rectF.contains(event.getX(),event.getY())) {
+                    float dx=event.getX()-startX;
+                    Log.e("x","sx:"+startX+"nowx:"+event.getX());
+                    changeVoice(dx);
+                    startX=event.getX();
+                }
+                break;
+        }
+        return true;
+    }
+
+    public void changeVoice(float dx){
+        Log.e("dx",dx+"");
+        int change=(int)(dx*100/rectF.width());
+        Log.e("change",change+"");
+        if(change+progress>=100){
+            progress=100;
+        }
+        else if(change+progress<=0){
+            progress=0;
+        }
+        else{
+            progress=progress+change;
+        }
+        invalidate();
+    }
+
 }
